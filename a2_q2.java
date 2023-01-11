94dc40aae1954ecb444f8465524e34c081dcfd14
Khushboo-Verma2004 <116653011+Khushboo-Verma2004@users.noreply.github.com>
2023-01-11 19:27:33 +0530

    Submitting answer of Q2 in Assignment1

---
+++ a2_q2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class a2_q2 {
+    public static void main(String[] args) { 
+      try (Scanner scn = new Scanner(System.in)) {
+        int marks = scn.nextInt(); 
+      
+         if (marks>90){ 
+      
+             System.out.println("Excellent."); 
+      
+         }else if (marks>80){ 
+      
+             System.out.println("good."); 
+      
+         }else if (marks>70){ 
+             System.out.println("fair."); 
+      
+         }else if (marks>60){ 
+      
+             System.out.println("Meets expectations."); 
+      
+         }else if(60>marks){ 
+      
+             System.out.println("Below par."); 
+      
+         }
+    } 
+      
+         } 
+      
+        } 
